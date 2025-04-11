eff5a4b3193e13c680a0327632f97b2b10386dcc
Parker Rex <parker.m.rex@gmail.com>
2025-04-11 15:03:39 -0400

    updated links for press and watch pages

--- src/app/watch/data.ts
+++ src/app/watch/data.ts
@@ -9,21 +9,23 @@ export interface ShowAppearance {
 
 export const showData: ShowAppearance[] = [
   {
-    tvNetwork: "HGTV",
+    tvNetwork: "Designing Miami",
     mainImage: "/watch/tv-1.jpeg",
-    showName: "Divided by Design",
+    showName: "Designing Miami",
     showDescription:
       "Husband and wife designers Ray and Eilyn Jimenez run their separate design firms and battle against each other in the very competitive Miami market, but no matter who lands the bid, their client always comes out a winner.",
     buttonText: "WATCH ON MAX",
-    watchUrl: "https://www.max.com/", // Placeholder URL - Update with actual link
+    watchUrl:
+      "https://www.max.com/shows/divided-by-design/e79f719c-8https://www.netflix.com/title/81425251", // Placeholder URL - Update with actual link
   },
   {
     tvNetwork: "Netflix",
     mainImage: "/watch/tv-2.jpeg",
-    showName: "Designing Miami",
+    showName: "Divided by Design",
     showDescription:
       "For Ray and Eilyn Jimenez, marriage and business are a perfect match as their respective interior design firms remodel homes for high-end Miami clients.",
     buttonText: "WATCH ON NETFLIX",
-    watchUrl: "https://www.netflix.com/", // Placeholder URL - Update with actual link
+    watchUrl:
+      "https://www.max.com/shows/divided-by-design/e79f719c-8651-4b2e-b0f8-4f075f7760e5", // Placeholder URL - Update with actual link
   },
 ];
