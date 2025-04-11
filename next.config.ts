70bec1cbf3c20cc1e82cbed6ccd2efdbe51a21a9
Parker Rex <parker.m.rex@gmail.com>
2025-04-11 09:27:07 -0400

    fix bundler

--- next.config.ts
+++ next.config.ts
@@ -8,18 +8,8 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    formats: ["image/avif", "image/webp"],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    domains: [],
-    path: "/_next/image",
-    minimumCacheTTL: 31536000,
-    dangerouslyAllowSVG: false,
-    contentDispositionType: "inline",
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    remotePatterns: [],
-    unoptimized: false,
+  outputFileTracingExcludes: {
+    "/_next/static": ["**/*"],
   },
 };
 
